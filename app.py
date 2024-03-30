b41ecd7be72ab5a57ee7a7565719f5f39ba29dd1
joshjabour <josh.jabour@oneacrefund.org>
2024-03-30 21:39:14 +0200

    feat: added error handling

--- app.py
+++ app.py
@@ -9,10 +9,10 @@ app = Flask(__name__)
 @app.route('/rates', methods=['GET']) 
 def get_price_averages():
     # Retrieve query parameters
-    date_from = request.args.get('date_from') # for testing, use '2016-01-01'
-    date_to = request.args.get('date_to') # for testing, use '2016-01-05'
-    origin = request.args.get('origin') # for testing, use 'CNSGH' (or 'china_main')
-    destination = request.args.get('destination') # for testing, use 'north_europe_main' (or 'NLRTM')
+    date_from = request.args.get('date_from')
+    date_to = request.args.get('date_to')
+    origin = request.args.get('origin')
+    destination = request.args.get('destination')
     
     # Check if date_from, date_to, origin, and destination are not empty
     if not date_from or not date_to or not origin or not destination:
@@ -23,6 +23,9 @@ def get_price_averages():
         datetime.datetime.strptime(date_to, '%Y-%m-%d')
     except ValueError:
         return jsonify({'Error': 'Invalid date format'}), 400
+    # Check if date_from is earlier than date_to
+    if date_from >= date_to:
+        return jsonify({'Error': 'Invalid date range. The date_from must be earlier than the date_to'}), 400
 
     originPortCodes = []
     destinationPortCodes = []
@@ -40,51 +43,64 @@ def get_price_averages():
 
         # Get the port codes for the origin and destination
         # First, determine whether the a port code or a region slug was supplied for the origin and destination
-        # ASSUMPTION: I've defined a port code as an upper-case string with 5 or fewer characters
-        if len(origin) <= 5 and origin.isupper(): # Port code was given
+        # Since we're dealing with UN/LOCODEs I defined a port code as an alphanumeric upper-case string with 5 
+        # or fewer characters
+        
+        # For origin:
+        if len(origin) <= 5 and origin.isupper():  # Port code was given
+            if not port_code_exists(origin, connection):
+                app.logger.error(f"Port code {origin} does not exist.")
+                return jsonify({"Error": f"Port code {origin} does not exist."}), 400
             originPortCodes.append(origin)
-        else: # Region slug was given. Get the port codes for the region and descendants
+        else:  # Region slug was given. Get the port codes for the region and descendants
+            if not slug_exists(origin, connection):
+                app.logger.error(f"Region {origin} does not exist.")
+                return jsonify({"Error": f"Region slug {origin} does not exist."}), 400
             originPortCodes = get_port_codes(origin, connection)
-        if len(destination) <= 5 and destination.isupper(): # Port code was given
-            destinationPortCodes.append(destination) 
-        else: # Region slug was given. Get the port codes for the region and descendants
+        
+        # For destination:
+        if len(destination) <= 5 and destination.isupper():  # Port code was given
+            if not port_code_exists(destination, connection):
+                app.logger.error(f"Port code {destination} does not exist.")
+                return jsonify({"Error": f"Port code {destination} does not exist."}), 400
+            destinationPortCodes.append(destination)
+        else:  # Region slug was given. Get the port codes for the region and descendants
+            if not slug_exists(destination, connection):
+                app.logger.error(f"Region {destination} does not exist.")
+                return jsonify({"Error": f"Region slug {destination} does not exist."}), 400
             destinationPortCodes = get_port_codes(destination, connection)
 
         # Get price averages across all ports for each day in the range supplied
-        cursor = connection.cursor()
-        cursor.execute('''
-            -- Generate a series of every day between date_from and date_to 
-            -- so that the results will include all days in the range even if
-            -- there are no records for a particular day in the prices table
-            WITH days AS (
-                SELECT GENERATE_SERIES(%s, %s, interval '1 day')::date AS day
-            )
-            -- Get the average price for each day in the range having at least 3
-            -- records, rounded to the nearest whole number
-            SELECT
-                days.day,
-                CASE
-                    WHEN COUNT(*) >= 3 THEN ROUND(AVG(price))
-                ELSE NULL
-                END AS average_price
-            FROM
-                days
-                LEFT JOIN prices ON 
-                    days.day = prices.day AND
-                    orig_code IN %s AND
-                    dest_code IN %s
-            GROUP BY days.day
-            ORDER BY days.day;
-            ''', (date_from, date_to, tuple(originPortCodes), tuple(destinationPortCodes)))
-        prices = cursor.fetchall()
-        cursor.close()
-
+        with connection.cursor() as cursor:
+            cursor.execute('''
+                -- Generate a series of every day between date_from and date_to 
+                -- so that the results will include all days in the range even if
+                -- there are no records for a particular day in the prices table
+                WITH days AS (
+                    SELECT GENERATE_SERIES(%s, %s, interval '1 day')::date AS day
+                )
+                -- Get the average price for each day in the range having at least 3
+                -- records, rounded to the nearest whole number
+                SELECT
+                    days.day,
+                    CASE
+                        WHEN COUNT(*) >= 3 THEN ROUND(AVG(price))
+                    ELSE NULL
+                    END AS average_price
+                FROM
+                    days
+                    LEFT JOIN prices ON 
+                        days.day = prices.day AND
+                        orig_code IN %s AND
+                        dest_code IN %s
+                GROUP BY days.day
+                ORDER BY days.day;
+                ''', (date_from, date_to, tuple(originPortCodes), tuple(destinationPortCodes)))
+            prices = cursor.fetchall()
     except psycopg2.Error as error:
         app.logger.error("Error fetching prices from the database: %s", error)
-        return jsonify({'Error': 'Failed to fetch prices from the database. Please try again later.'}), 500
+        return jsonify({'Error': 'Failed to fetch prices from the database. Please check your origin and destination.'}), 500
     finally:
-        if cursor is not None:
-            cursor.close()
         if connection is not None:
             connection.close()
     
@@ -104,33 +120,46 @@ def get_price_averages():
     # Create a Flask Response object and set the content type to 'application/json'
     return Response(response_json, content_type='application/json; charset=utf-8')
 
+# Checks if the port code exists in the ports table and returns true or false
+def port_code_exists(port_code, connection):
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT EXISTS(SELECT 1 FROM ports WHERE code = %s);", (port_code,))
+        exists = cursor.fetchone()[0]
+        return exists
+
+# Checks if the slug exists in the regions table and returns true or false
+def slug_exists(slug, connection):
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT EXISTS(SELECT 1 FROM regions WHERE slug = %s);", (slug,))
+        exists = cursor.fetchone()[0]
+        return exists
+
 # Retrieves the port codes for a given region slug and descendants.
 # It uses a recursive query to traverse the region hierarchy.
 # The function takes two parameters: the region slug and the database connection object.
 # It returns a list of port codes associated with the given region.
 def get_port_codes(region_slug, connection):
-    cursor = None
     port_codes = []
-    cursor = connection.cursor()
-    cursor.execute('''
-        WITH RECURSIVE region_tree AS (
-            SELECT slug, parent_slug
-            FROM regions
-            WHERE slug = %s
+    with connection.cursor() as cursor:
+        cursor.execute('''
+            WITH RECURSIVE region_tree AS (
+                SELECT slug, parent_slug
+                FROM regions
+                WHERE slug = %s
 
-            UNION ALL
+                UNION ALL
 
-            SELECT regions.slug, regions.parent_slug
-            FROM regions
-            JOIN region_tree ON regions.parent_slug = region_tree.slug
-        )
-        SELECT ports.code
-        FROM ports 
-        INNER JOIN region_tree ON ports.parent_slug = region_tree.slug;
-        ''', (region_slug,))
-    port_codes = cursor.fetchall()
-    cursor.close()
-    return port_codes
+                SELECT regions.slug, regions.parent_slug
+                FROM regions
+                JOIN region_tree ON regions.parent_slug = region_tree.slug
+            )
+            SELECT ports.code
+            FROM ports 
+            INNER JOIN region_tree ON ports.parent_slug = region_tree.slug;
+            ''', (region_slug,))
+        port_codes = cursor.fetchall()
+        cursor.close()
+        return port_codes
 
 if __name__ == "__main__":
     app.run(debug=True)
